d60b6783ea3769c9b54a3cb619ff54ca99f96c17
Pitpower <37909142+Pitpower@users.noreply.github.com>
2018-10-20 09:34:20 +0200

    Add marker method

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
-import { Geolocation } from '@ionic-native/geolocation';
+import {Geolocation, GeolocationOptions} from '@ionic-native/geolocation';
 
 declare var google;
 
@@ -13,6 +13,7 @@ export class MapPage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   latLng: any;
+
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public plt: Platform,
@@ -38,6 +39,55 @@ export class MapPage {
 
     // Create the map with the options defined
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+   this.addMarker();
+    this.addMarker2();
   }
+
+  addMarker() {
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+
+  }
+
+  addMarker2() {
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: new google.maps.LatLng("39.682327", "-0.277353")
+      //  {lat:39.682327, long:-0.277353}
+    });
+
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+
+  }
+
+  addInfoWindow(marker, content) {
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
+
+
+
+
+
+
 
 }
