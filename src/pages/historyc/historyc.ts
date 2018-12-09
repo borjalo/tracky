106c4297354cf8b54aa91e95067df871593b43ec
Lois Reinhard <loisrgil@gmail.com>
2018-12-09 15:48:25 +0100

    Added page, minor menu changes
    Added history page. Minor changes to main page to account for menu items and removed features of the previous order list.

--- src/pages/historyc/historyc.ts
+++ src/pages/historyc/historyc.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FirebaseService } from "../../app/services/firebase-service";
 import {Subscription} from "rxjs";
 import {userToken} from "../../app/services/userToken";
 
-
 @IonicPage()
 @Component({
-  selector: 'page-order-list',
-  templateUrl: 'order-list.html',
+  selector: 'page-history',
+  templateUrl: 'historyc.html',
 })
 
-export class OrderListPage implements OnInit {
+export class HistorycPage implements OnInit {
   public items: Array<any> = [];
   private orders: any = [];
   private subscripcion:Subscription;
@@ -27,16 +26,16 @@ export class OrderListPage implements OnInit {
       if(this.userLogin.getLogin().tipo=="deliveryman"){
         var list= [];
         for(let order of res){
-         if((this.userLogin.getLogin().nombre==order.deliveryman && order.state!="Entregado")){
-           list.push(order);
-         }
+          if((this.userLogin.getLogin().nombre==order.deliveryman && order.state=="Entregado")){
+            list.push(order);
+          }
         }
         this.orders=list;
       }
       else {
         list= [];
         for(let order of res){
-          if(order.state!="Entregado"){
+          if(order.state=="Entregado"){
             list.push(order);
           }
         }
@@ -44,7 +43,7 @@ export class OrderListPage implements OnInit {
       }
     });
   }
-ngOnDestroy(){
+  ngOnDestroy(){
     this.subscripcion.unsubscribe();
   }
 
@@ -60,3 +59,4 @@ ngOnDestroy(){
   }
 
 }
+
